d11481542c9423b3827f4ef4cf347f8ace12dc3a
josephdeeb <josephdbesenski@gmail.com>
2021-11-21 14:25:21 -0700

    Implemented readData function to get coordinates from text file

--- EntropyProcessor.java
+++ EntropyProcessor.java
@@ -1,17 +1,21 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EntropyProcessor {
 	
 	/*
 	 * NOTE: Any additional information we need on the mouse inputs (timing for instance) can be added to the Pair class
 	 */
-
 	public static void main(String[] args) {
-		if (args.length != 3) {
+		if (args.length != 2) {
 			System.err.println("ERROR: Must supply filename and confidence value as a command line argument");
+			System.exit(-1);
 		}
-		String filename = args[1];
-		double confidence = Double.parseDouble(args[2]);
+		String filename = args[0];
+		double confidence = Double.parseDouble(args[1]);
 		
 		ArrayList<Pair> data = readData(filename);
 		
@@ -28,15 +32,36 @@ public class EntropyProcessor {
 	
 	/*
 	 * Reads the filename and creates pairs of integers to represent the mouse data from that file
+	 * Implemented by Joseph
 	 */
 	public static ArrayList<Pair> readData(String filename) {
-		return null;
+		ArrayList<Pair> pairs = new ArrayList<Pair>();
+		try {
+			File coordinates = new File(filename);
+			Scanner reader = new Scanner(coordinates);
+			Pattern p = Pattern.compile("\\d+");
+			Matcher m = null;
+			int x = 0, y = 0;
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
+				m = p.matcher(line);
+				m.find();
+				x = Integer.parseInt(m.group());
+				m.find();
+				y = Integer.parseInt(m.group());
+				pairs.add(new Pair(x, y));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: coordinates.txt not found");
+			System.exit(-1);
+		}
+		return pairs;
 	}
 	
 	/*
 	 * Takes an array list of pairs of integers representing (X, Y) mouse coordinates and processes the data
 	 * For instance, removing pairs at (0, 0).
-	 * Returns a processed ArrayList of pairs of integers representing (X, Y) mouse coordinates
+	 * Returns a processed ArrayList of pairs of xintegers representing (X, Y) mouse coordinates
 	 */
 	public static ArrayList<Pair> process(ArrayList<Pair> data) {
 		return data;
@@ -44,6 +69,7 @@ public class EntropyProcessor {
 	
 	/*
 	 * Takes an ArrayList of pairs of integers representing (X, Y) mouse coordinates and returns a double representing the estimated amount of entropy from the data
+	 * Implemented by Joseph
 	 */
 	public static double estimate(ArrayList<Pair> data) {
 		return 0.0;
