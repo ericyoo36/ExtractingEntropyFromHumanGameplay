bfd8ded53e781d899466b4c6e05de3d0c69cf387
Zadanz <41347579+Zadanz@users.noreply.github.com>
2021-11-29 16:46:17 -0700

    Added blockFreqNIST, blockRunsNIST, universalNIST
    Implemented new NIST test functions.

--- EntropyProcessor.java
+++ EntropyProcessor.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
-import org.apache.commons.math3.special.Erf; //erfc function for Monobit test
+import org.apache.commons.math3.special.*; //erfc function for Monobit test, Gamma for block tests
 import org.jtransforms.fft.DoubleFFT_1D; //Fourier Transform function for NIST test
 
 public class EntropyProcessor {
@@ -416,8 +416,8 @@ public class EntropyProcessor {
 		double X[] = new double[length];
 		
 		for (int i = 0; i < length; i++) {
-            X[i] = 2 * (int) Character.getNumericValue(bits.charAt(i)) - 1;
-        }
+            		X[i] = 2 * (int) Character.getNumericValue(bits.charAt(i)) - 1;
+		}
 		
 		DoubleFFT_1D fft = new DoubleFFT_1D(length);
 		fft.realForward(X);
@@ -428,25 +428,25 @@ public class EntropyProcessor {
 		M[length/2] = Math.sqrt(X[1] * X[1]);
 		
 		for (int i = 0; i < length / 2 - 1; i++) {
-            M[i + 1] = Math.hypot(X[2 * i + 2], X[2 * i + 3]);
-        }
+            		M[i + 1] = Math.hypot(X[2 * i + 2], X[2 * i + 3]);
+       		 }
 		
 		int count = 0;
-        double upperBound = Math.sqrt(Math.log(1/0.05) * length);
-        for (int i = 0; i < length / 2; i++) {
-            if (M[i] < upperBound) {
-                count++;
-            }
-        }
+        	double upperBound = Math.sqrt(Math.log(1/0.05) * length);
+        	for (int i = 0; i < length / 2; i++) {
+            		if (M[i] < upperBound) {
+                		count++;
+            		}
+		}
         
-        double N1 = count;
-        double N0 = 0.95 * length / 2.0;
-        
-        d = (N1 - N0) / Math.sqrt(length * 0.95 * 0.05 / 4);
-        
-        double pvalue = Erf.erfc(Math.abs(d) / Math.sqrt(2));
-        
-        if (pvalue < confidence) {
+		double N1 = count;
+		double N0 = 0.95 * length / 2.0;
+
+		d = (N1 - N0) / Math.sqrt(length * 0.95 * 0.05 / 4);
+
+		double pvalue = Erf.erfc(Math.abs(d) / Math.sqrt(2));
+
+        	if (pvalue < confidence) {
 			System.out.println("P-value: " + pvalue);
 			System.out.println("Null hypothesis rejected (Not random).");
 			return false;
@@ -461,67 +461,107 @@ public class EntropyProcessor {
 	public static boolean cusumNIST (String bits, int mode, double confidence) {
 		System.out.println("NIST Cumulative Sum Test:");
 		int z;
-        int zrev;
-        int n = bits.length();
-        double sqrtN = Math.sqrt(n);
-        int S, sup, inf, k;
-        double sum1, sum2;
-        
-        final NormalDistribution nd = new NormalDistribution();
+		int zrev;
+		int n = bits.length();
+		double sqrtN = Math.sqrt(n);
+		int S, sup, inf, k;
+		double sum1, sum2;
 
-        z = zrev = 0;
-        S = 0;
-        sup = 0;
-        inf = 0;
-        for (k = 0; k < n; k++) {
-            if (Character.getNumericValue(bits.charAt(k)) != 0) {
-                S++;
-            } else {
-                S--;
-            }
-            if (S > sup) {
-                sup++;
-            }
-            if (S < inf) {
-                inf--;
-            }
-            z = (sup > -inf) ? sup : -inf;
-            zrev = (sup - S > S - inf) ? sup - S : S - inf;
-        }
-        
-        double pvalue;
+		final NormalDistribution nd = new NormalDistribution();
 
-        if (mode == 0) {
-	        // forward
-	        sum1 = 0.0;
-	        for (k = (-n / z + 1) / 4; k <= (n / z - 1) / 4; k++) {
-	            sum1 += nd.cumulativeProbability(((4 * k + 1) * z) / sqrtN);
-	            sum1 -= nd.cumulativeProbability(((4 * k - 1) * z) / sqrtN);
-	        }
-	        sum2 = 0.0;
-	        for (k = (-n / z - 3) / 4; k <= (n / z - 1) / 4; k++) {
-	            sum2 += nd.cumulativeProbability(((4 * k + 3) * z) / sqrtN);
-	            sum2 -= nd.cumulativeProbability(((4 * k + 1) * z) / sqrtN);
-	        }
+		z = zrev = 0;
+		S = 0;
+		sup = 0;
+		inf = 0;
+		for (k = 0; k < n; k++) {
+		    if (Character.getNumericValue(bits.charAt(k)) != 0) {
+			S++;
+		    } else {
+			S--;
+		    }
+		    if (S > sup) {
+			sup++;
+		    }
+		    if (S < inf) {
+			inf--;
+		    }
+		    z = (sup > -inf) ? sup : -inf;
+		    zrev = (sup - S > S - inf) ? sup - S : S - inf;
+		}
+        
+		double pvalue;
+
+		if (mode == 0) {
+			// forward
+			sum1 = 0.0;
+			for (k = (-n / z + 1) / 4; k <= (n / z - 1) / 4; k++) {
+			    sum1 += nd.cumulativeProbability(((4 * k + 1) * z) / sqrtN);
+			    sum1 -= nd.cumulativeProbability(((4 * k - 1) * z) / sqrtN);
+			}
+			sum2 = 0.0;
+			for (k = (-n / z - 3) / 4; k <= (n / z - 1) / 4; k++) {
+			    sum2 += nd.cumulativeProbability(((4 * k + 3) * z) / sqrtN);
+			    sum2 -= nd.cumulativeProbability(((4 * k + 1) * z) / sqrtN);
+			}
+
+			pvalue = 1.0 - sum1 + sum2;
+		}
+		else {
+			// backwards
+			sum1 = 0.0;
+			for (k = (-n / zrev + 1) / 4; k <= (n / zrev - 1) / 4; k++) {
+			    sum1 += nd.cumulativeProbability(((4 * k + 1) * zrev) / sqrtN);
+			    sum1 -= nd.cumulativeProbability(((4 * k - 1) * zrev) / sqrtN);
+			}
+			sum2 = 0.0;
+			for (k = (-n / zrev - 3) / 4; k <= (n / zrev - 1) / 4; k++) {
+			    sum2 += nd.cumulativeProbability(((4 * k + 3) * zrev) / sqrtN);
+			    sum2 -= nd.cumulativeProbability(((4 * k + 1) * zrev) / sqrtN);
+			}
+			pvalue = 1.0 - sum1 + sum2;
+		}
+
+		if (pvalue < confidence) {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis rejected (Not random).");
+			return false;
+		}
+		else {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis accepted (Sufficiently random).");
+			return true;
+			}
+		}
 	
-	        pvalue = 1.0 - sum1 + sum2;
-        }
-        else {
-	        // backwards
-	        sum1 = 0.0;
-	        for (k = (-n / zrev + 1) / 4; k <= (n / zrev - 1) / 4; k++) {
-	            sum1 += nd.cumulativeProbability(((4 * k + 1) * zrev) / sqrtN);
-	            sum1 -= nd.cumulativeProbability(((4 * k - 1) * zrev) / sqrtN);
-	        }
-	        sum2 = 0.0;
-	        for (k = (-n / zrev - 3) / 4; k <= (n / zrev - 1) / 4; k++) {
-	            sum2 += nd.cumulativeProbability(((4 * k + 3) * zrev) / sqrtN);
-	            sum2 -= nd.cumulativeProbability(((4 * k + 1) * zrev) / sqrtN);
-	        }
-	        pvalue = 1.0 - sum1 + sum2;
-        }
-		
-        if (pvalue < confidence) {
+	public static boolean blockFreqNIST (String bits, int blockLen, double confidence) {
+		System.out.println("NIST Frequency Test within a Block:");
+		double chi_squared;
+		int blockCount;
+
+		int n = bits.length();
+		int discarded = n % blockLen;
+
+		int i, j, blockSum;
+		double sum, pi, v;
+
+		blockCount = n / blockLen;
+		/* # OF SUBSTRING BLOCKS      */
+		sum = 0.0;
+
+		for (i = 0; i < blockCount; i++) {
+		    blockSum = 0;
+		    int offset = i * blockLen;
+		    for (j = 0; j < blockLen; j++) {
+			blockSum += Character.getNumericValue(bits.charAt((offset + j)));
+		    }
+		    pi = (double) blockSum / (double) blockLen;
+		    v = pi - 0.5;
+		    sum += v * v;
+		}
+		chi_squared = 4.0 * blockLen * sum;
+		double pvalue = Gamma.regularizedGammaQ(blockCount/2.0, chi_squared/2.0);
+
+		if (pvalue < confidence) {
 			System.out.println("P-value: " + pvalue);
 			System.out.println("Null hypothesis rejected (Not random).");
 			return false;
@@ -532,4 +572,221 @@ public class EntropyProcessor {
 			return true;
 		}
 	}
+	
+	public static boolean blockRunsNIST (String bits, double confidence) {
+		System.out.println("NIST Test for the Longest Run of Ones in a Block:");
+		int blockLen;
+		int numberOfBlocks;
+		double chi2;
+		int K;
+
+		int length = bits.length();
+		double pi[] = new double[7];
+		int run, v_n_obs, i, j, V[] = new int[7];
+
+		int [] nu = new int[]{ 0, 0, 0, 0, 0, 0, 0 };
+
+		if (length < 128) {
+			System.out.println("Sequence too short.");
+	    		return false;
+		}
+		if (length < 6272) {
+		    K = 3;
+		    blockLen = 8;
+		    V[0] = 1;
+		    V[1] = 2;
+		    V[2] = 3;
+		    V[3] = 4;
+		    pi[0] = 0.21484375;
+		    pi[1] = 0.3671875;
+		    pi[2] = 0.23046875;
+		    pi[3] = 0.1875;
+		} else if (length < 750000) {
+		    K = 5;
+		    blockLen = 128;
+		    V[0] = 4;
+		    V[1] = 5;
+		    V[2] = 6;
+		    V[3] = 7;
+		    V[4] = 8;
+		    V[5] = 9;
+		    pi[0] = 0.1174035788;
+		    pi[1] = 0.242955959;
+		    pi[2] = 0.249363483;
+		    pi[3] = 0.17517706;
+		    pi[4] = 0.102701071;
+		    pi[5] = 0.112398847;
+		} else {
+		    K = 6;
+		    blockLen = 10000;
+		    V[0] = 10;
+		    V[1] = 11;
+		    V[2] = 12;
+		    V[3] = 13;
+		    V[4] = 14;
+		    V[5] = 15;
+		    V[6] = 16;
+		    pi[0] = 0.0882;
+		    pi[1] = 0.2092;
+		    pi[2] = 0.2483;
+		    pi[3] = 0.1933;
+		    pi[4] = 0.1208;
+		    pi[5] = 0.0675;
+		    pi[6] = 0.0727;
+		}
+
+		numberOfBlocks = length / blockLen;
+		for (i = 0; i < numberOfBlocks; i++) {
+		    v_n_obs = 0;
+		    run = 0;
+		    for (j = 0; j < blockLen; j++) {
+			if (Character.getNumericValue(bits.charAt((i * blockLen + j))) == 1) {
+			    run++;
+			    if (run > v_n_obs) {
+				v_n_obs = run;
+			    }
+			} else {
+			    run = 0;
+			}
+		    }
+		    if (v_n_obs < V[0]) {
+			nu[0]++;
+		    }
+		    for (j = 0; j <= K; j++) {
+			if (v_n_obs == V[j]) {
+			    nu[j]++;
+			}
+		    }
+		    if (v_n_obs > V[K]) {
+			nu[K]++;
+		    }
+		}
+
+		chi2 = 0.0;
+		for (i = 0; i <= K; i++) {
+		    chi2 += ((nu[i] - numberOfBlocks * pi[i]) * (nu[i] - numberOfBlocks * pi[i])) / (numberOfBlocks * pi[i]);
+		}
+
+		double pvalue = Gamma.regularizedGammaQ((double) (K / 2.0), chi2 / 2.0);
+
+		if (pvalue < confidence) {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis rejected (Not random).");
+			return false;
+		}
+		else {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis accepted (Sufficiently random).");
+			return true;
+		}
+	}
+	
+	public static boolean universalNIST (String bits, int numBlocks, int blockLength, double confidence) {
+		System.out.println("NIST Universal Statistical Test:");
+		int length = bits.length();
+
+		int i, j, p;
+		double arg, sqrt2, c;
+		long T[];
+		int decRep;
+
+		final double variance[] = { 0, 0, 0, 0, 0, 0, 2.954, 3.125, 3.238, 3.311, 3.356, 3.384,
+			3.401, 3.410, 3.416, 3.419, 3.421 };
+
+		final double expected_value[] = { 0, 0, 0, 0, 0, 0, 5.2177052, 6.1962507, 7.1836656,
+			      8.1764248, 9.1723243, 10.170032, 11.168765,
+			      12.168070, 13.167693, 14.167488, 15.167379 };
+
+		int L = 5;
+		if (length >= 387840) {
+		    L = 6;
+		}
+		if (length >= 904960) {
+		    L = 7;
+		}
+		if (length >= 2068480) {
+		    L = 8;
+		}
+		if (length >= 4654080) {
+		    L = 9;
+		}
+		if (length >= 10342400) {
+		    L = 10;
+		}
+		if (length >= 22753280) {
+		    L = 11;
+		}
+		if (length >= 49643520) {
+		    L = 12;
+		}
+		if (length >= 107560960) {
+		    L = 13;
+		}
+		if (length >= 231669760) {
+		    L = 14;
+		}
+		if (length >= 496435200) {
+		    L = 15;
+		}
+		if (length >= 1059061760) {
+		    L = 16;
+		}
+
+		int Q = 10 * (int) (1 << L);
+		int K = (int) (Math.floor(length / L) - (double) Q);
+		/* BLOCKS TO TEST */
+
+		p = (int) 1 << L; // pow(2, L);
+
+		T = new long[p];
+
+		if ((L < 6) || (L > 16) || ((double) Q < 10 * (1 << L))) {
+			System.out.println("Poor conditions for test.");
+		    return false;
+		}
+
+		/* COMPUTE THE EXPECTED:  Formula 16, in Marsaglia's Paper */
+		c = 0.7 - 0.8 / (double) L + (4 + 32 / (double) L) * Math.pow(K, -3 / (double) L) / 15;
+		double sigma = c * Math.sqrt(variance[L] / (double) K);
+		sqrt2 = Math.sqrt(2);
+		double log2 = Math.log(2);
+		double sum = 0.0;
+
+		for (i = 0; i < p; i++) {
+		    T[i] = 0;
+		}
+		for (i = 1; i <= Q; i++) {
+		    /* INITIALIZE TABLE */
+		    decRep = 0;
+		    for (j = 0; j < L; j++) {
+			decRep += Character.getNumericValue(bits.charAt((i - 1) * L + j)) * (long) Math.pow(2, L - 1 - j);
+		    }
+		    T[decRep] = (long) i;
+		}
+		for (i = Q + 1; i <= Q + K; i++) {
+		    /* PROCESS BLOCKS */
+		    decRep = 0;
+		    for (j = 0; j < L; j++) {
+			decRep += Character.getNumericValue(bits.charAt((i - 1) * L + j)) * (long) Math.pow(2, L - 1 - j);
+		    }
+		    sum += Math.log((double) i - T[decRep]) / log2;
+		    T[decRep] = i;
+		}
+		double phi = (double) (sum / (double) K);
+		arg = Math.abs(phi - expected_value[L]) / (sqrt2 * sigma);
+
+		double pvalue = Erf.erfc(arg);
+
+		if (pvalue < confidence) {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis rejected (Not random).");
+			return false;
+		}
+		else {
+			System.out.println("P-value: " + pvalue);
+			System.out.println("Null hypothesis accepted (Sufficiently random).");
+			return true;
+		}
+	}
+	
 }
